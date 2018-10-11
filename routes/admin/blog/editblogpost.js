e0fd623bd48270b5fcdd4c413e6e59f1af2e1315
william341 <wwokwilliam@gmail.com>
2018-10-10 19:17:45 -0700

    Bug fixes

--- routes/admin/blog/editblogpost.js
+++ routes/admin/blog/editblogpost.js
@@ -5,6 +5,12 @@ var User = require('../../../lib/user');
 var crypto = require('crypto');
 var router = express.Router();
 var Post = require('mongoose').model('Post');
+var Topbar = require('mongoose').model('Topbar');
+
+var topbardata = new Topbar({name:"Whoops! We did a fucksy wucksy. The navbar system is broken!", url: "/"});
+Topbar.find({}, (err, links) => {
+    topbardata = links;
+});
 
 router.get('/:id', function(req, res) {
     if(req.session.user === undefined) {
@@ -12,7 +18,7 @@ router.get('/:id', function(req, res) {
     }
     if(req.session.user.permissionlvl === 255) {
         Post.findById({_id: req.params.id}, (err, posts) => {
-            return res.render('blog/editblogpost', {post: posts, usersession: req.session.user});
+            return res.render('blog/editblogpost', {post: posts, usersession: req.session.user, links: topbardata});
         });
     } else {
         return res.status(403).send();
